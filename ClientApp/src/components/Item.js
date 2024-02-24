9834fc237755c55bba4fcd7f155aba4512b96f97
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-02-24 16:03:07 +0100

    final version

---
+++ ClientApp/src/components/Item.js
@@ -0,0 +1,12 @@
+﻿/* eslint-disable jsx-a11y/alt-text */
+const Item = ({ item, drag, itemImgObj }) => {
+    return (
+        <div className="unranked-cell">
+            <img id={`item-${item.id}`} src={itemImgObj.image}
+                style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
+            />
+        </div> 
+    )
+
+}
+export default Item;
